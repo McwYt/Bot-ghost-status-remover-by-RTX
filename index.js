8c2be894b0adf79c98a3b12e53f7083faa8ce8ad
McwYt <157890359+McwYt@users.noreply.github.com>
2024-01-27 17:57:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching ?help | McwYt Discord Bot","Listening McwYt Videos","McwYt"];
 
 
 let currentIndex = 0;
